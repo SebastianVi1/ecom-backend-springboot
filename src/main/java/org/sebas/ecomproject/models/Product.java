724b30605e6b1220376145b18c76e40d3d792b64
Andre <sebastianvh86@gmail.com>
2025-07-06 15:49:16 -0600

    add Product model with fields and annotations

---
+++ src/main/java/org/sebas/ecomproject/models/Product.java
@@ -0,0 +1,31 @@
+package org.sebas.ecomproject.models;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String desc;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private boolean aviable;
+    private int quantity;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date releaseDate;
+
+}
